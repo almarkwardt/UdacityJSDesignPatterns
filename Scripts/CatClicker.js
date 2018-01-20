269b976a20d8bdf91e39ea6c4da638a5cad9918a
Aaron Markwardt <16973065+almarkwardt@users.noreply.github.com>
2018-01-20 13:09:57 -0600

    Cat clicker 2 (lesson 1, through section 8.)
    Notes:
     - Requirements:
       - Shall support two cats.
       - Shall display cat's name, number of clicks per cat, and photo.
     - Second photo from https://www.pexels.com/photo/eyes-cat-coach-sofa-96938/

--- Scripts/CatClicker.js
+++ Scripts/CatClicker.js
@@ -1,8 +1,35 @@
-clickCount = 0;
+cat1ClickCount = 0;
+cat1Name = "Dr. Fuzzles";
+cat2ClickCount = 0;
+cat2Name = "CatFace"
 
-var catPhotoElement = document.getElementById("TheCatPhoto");
-catPhotoElement.addEventListener("click", function() {
-	var counterElement = document.getElementById("TheClickCounter");
-	clickCount++;
+var UpdateCat = function(catDOMElement, name, clickCount) {
+	var nameElement = catDOMElement.getElementsByClassName("Name")[0];
+	nameElement.textContent = name;
+	var counterElement = catDOMElement.getElementsByClassName("Counter")[0];
 	counterElement.textContent = clickCount;
+};
+
+var UpdatePage = function() {
+	var cat1Element = document.getElementById("Cat1");
+	UpdateCat(cat1Element, cat1Name, cat1ClickCount);
+
+	var cat2Element = document.getElementById("Cat2");
+	UpdateCat(cat2Element, cat2Name, cat2ClickCount);
+};
+
+var cat1PhotoElement = document.getElementById("Cat1").getElementsByClassName("Photo")[0];
+cat1PhotoElement.addEventListener("click", function() {
+	cat1ClickCount++;
+	UpdatePage();
+});
+
+var cat2PhotoElement = document.getElementById("Cat2").getElementsByClassName("Photo")[0];
+cat2PhotoElement.addEventListener("click", function() {
+	cat2ClickCount++;
+	UpdatePage();
+});
+
+document.addEventListener("DOMContentLoaded", function() {
+	UpdatePage();
 });
